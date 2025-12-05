d30d04587980213adc10c98961f07130e92b0a90
Miguel Gomes <a55217@alunos.ipb.pt>
2025-12-05 12:28:16 +0000

    4.0

--- test.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-let testPath = "templates";  // Simulate your issue
-console.log(path.join(__dirname, testPath));  // This will throw the error
-console.log("ola");  // This will throw the error
